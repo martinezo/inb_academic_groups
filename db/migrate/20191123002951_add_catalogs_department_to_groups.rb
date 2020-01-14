c4ffaec4270369f76b7472b714fc5689ee203361
Ramon Martinez Olvera <martinezo@inb.unam.mx>
2020-01-14 13:28:42 -0600

    clean an sort migrations

--- db/migrate/20191123002951_add_catalogs_department_to_groups.rb
+++
@@ -1,5 +0,0 @@
-class AddCatalogsDepartmentToGroups < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :groups, :catalogs_department, null: false, foreign_key: true
-  end
-end
--- db/migrate/20191125181021_add_rank_to_catalogs_member_types.rb
+++
@@ -1,5 +0,0 @@
-class AddRankToCatalogsMemberTypes < ActiveRecord::Migration[6.0]
-  def change
-    add_column :catalogs_member_types, :rank, :integer
-  end
-end
